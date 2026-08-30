3d06637ab1bc3ef1a88ee4235073fe38d8d68d1e
lj05117 <lj05117@naver.com>
2026-08-30 14:46:15 +0900

    init: 프로젝트 세팅

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import "./App.css";
+
+gsap.registerPlugin(ScrollTrigger);
+function App() {
+  return <div></div>;
+}
+
+export default App;
